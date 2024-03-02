9bb0dac4fe96e6a6b90f362a5b5b1fe492c2835a
trkien2510 <trkien2503@gmail.com>
2024-03-02 07:52:24 +0700

    add some button

--- Assets/Scripts/eventManager.cs
+++ Assets/Scripts/eventManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class eventManager : MonoBehaviour
 {
-    public void loadLevel1(){
-        SceneManager.LoadScene("Level 1");
+    public void loadNextBuildIndex(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void loadBackBuildIndex(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void loadMainScene(){
+        SceneManager.LoadScene("Main Scene");
     }
     public void loadMenuLevel(){
         SceneManager.LoadScene("Menu Level");
     }
-    public void showAbout(){
-
+    public void loadLevel1(){
+        SceneManager.LoadScene("Level 1");
+    }
+    public void loadLevel2(){
+        SceneManager.LoadScene("Level 2");
+    }
+    public void loadLevel3(){
+        SceneManager.LoadScene("Level 3");
+    }
+    public void openFB(){
+        Application.OpenURL("https://www.facebook.com/TrungKien25102k3");
     }
     public void exitWindow(){
         Application.Quit();
